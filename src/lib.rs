dbeb79036684d4475dcf00980e53b93f6d081ec3
Lennard Behrens <dev.behrenle@posteo.de>
2024-10-06 16:28:44 +0200

    refactor: :art: replace for loop with filter_map

--- src/lib.rs
+++ src/lib.rs
@@ -8,33 +8,40 @@ pub struct Match {
     candidate: String,
 }
 
-pub fn fuzzy_match(query: &str, candidates: Vec<&str>, threshold: f64) -> Vec<Match> {
-    let mut matches: Vec<Match> = Vec::new();
+pub fn fuzzy_match(query: &str, candidates: &Vec<&str>, threshold: f64) -> Vec<Match> {
+    let mut matches = candidates
+        .iter()
+        .filter_map(|candidate| {
+            // TODO:
+            // Rather run Levenshtein matching for every word in query string against every word in candidate
+            // Reasoning: Be more sensitive towards exact matches
+            // Split query
+            // Split candidate
+            // For each query substring, compare with each candidate substring
 
-    for candidate in &candidates {
-        // TODO:
-        // Rather run Levenshtein matching for every word in query string against every word in candidate
-        // Reasoning: Be more sensitive towards exact matches
-        // Split query
-        // Split candidate
-        // For each query substring, compare with each candidate substring
+            let levenshtein_distance = levenshtein::get_distance(query, &candidate, 10, 3, 1);
+            let n_gram_similarity = n_gram::get_similarity(query, &candidate, 3);
+            // Include exact matches score
 
-        let levenshtein_distance = levenshtein::get_distance(query, &candidate, 10, 3, 1);
-        let n_gram_similarity = n_gram::get_similarity(query, &candidate, 3);
-        // Include exact matches score
+            let score =
+                3.0 * (1 - levenshtein_distance / candidate.len()) as f64 + 7.0 * n_gram_similarity;
 
-        let score =
-            3.0 * (1 - levenshtein_distance / candidate.len()) as f64 + 7.0 * n_gram_similarity;
+            if score <= threshold {
+                Some(Match {
+                    score,
+                    candidate: candidate.to_string(),
+                })
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<_>>();
 
-        if score <= threshold {
-            matches.push(Match {
-                score,
-                candidate: candidate.to_string(),
-            });
-        }
-    }
-
-    matches.sort_by(|a, b| a.score.partial_cmp(&b.score).unwrap());
+    matches.sort_by(|a, b| {
+        a.score
+            .partial_cmp(&b.score)
+            .expect("scores should be comparable")
+    });
 
     matches
 }
@@ -50,7 +57,7 @@ mod tests {
         ];
         let query = "kitten";
         assert_eq!(
-            fuzzy_match(query, candidates, 10.),
+            fuzzy_match(query, &candidates, 10.),
             vec![
                 Match {
                     score: 0.0,
